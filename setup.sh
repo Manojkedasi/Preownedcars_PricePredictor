4987bf04d28ef1a37a963de33b55767c35a6524b
Manojkedasi <113597918+Manojkedasi@users.noreply.github.com>
2022-09-17 18:12:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
-mkdir ~p ~/.streamlit/
-
+mkdir -p ~/.streamlit/
 echo "\
-[general]\n\n
-email=\"your-email@domain.com\"\n\
+[general]\n\
+email = \"your@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
-handless = true\n\
+headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-
